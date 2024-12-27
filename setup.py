b244b31edf9c2806103b0d89ed351d732d3316fe
kairos <joaoslopes@gmail.com>
2024-12-27 00:31:23 +0000

    Automatic commit before Assistant edits

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    description="A Python package for managing and visualizing interval tree structures",
+    description=
+    "A Python package for managing and visualizing interval tree structures",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Replit User",
-    author_email="user@example.com",
-    url="https://github.com/user/tree-interval",
+    author="Joao Lopes",
+    author_email="joaslopes@gmail.com",
+    url="https://github.com/kairos-xx/tree-interval",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
