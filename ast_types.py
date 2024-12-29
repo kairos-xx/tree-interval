efdf54e4b140f4c92c21d823c23ec89695b7e756
kairos <joaoslopes@gmail.com>
2024-12-29 22:50:07 +0000

    Assistant checkpoint: Update AST types with structured information
    Assistant generated file changes:
    - ast_types.py: Update AST types with description and statement flags
    
    ---
    
    User prompt:
    
    each dictionary key should have a value of {"description":"...","statement":True/False}

--- ast_types.py
+++ ast_types.py
@@ -1,78 +1,243 @@
-
-# S - Creates a new statement
-# E - Part of an expression
 AST_TYPES = {
     # Module level
-    "Module": "Root node for entire file [S]",
-    "Import": "Import statement [S]",
-    "ImportFrom": "From import statement [S]",
-    
+    "Module": {
+        "description": "Root node for entire file",
+        "statement": True
+    },
+    "Import": {
+        "description": "Import statement",
+        "statement": True
+    },
+    "ImportFrom": {
+        "description": "From import statement",
+        "statement": True
+    },
+
     # Function and Class
-    "FunctionDef": "Function definition [S]",
-    "AsyncFunctionDef": "Async function definition [S]", 
-    "ClassDef": "Class definition [S]",
-    "Return": "Return statement [S]",
-    "Args": "Function arguments [E]",
-    "arguments": "Function argument definitions [E]",
-    
+    "FunctionDef": {
+        "description": "Function definition",
+        "statement": True
+    },
+    "AsyncFunctionDef": {
+        "description": "Async function definition",
+        "statement": True
+    },
+    "ClassDef": {
+        "description": "Class definition",
+        "statement": True
+    },
+    "Return": {
+        "description": "Return statement",
+        "statement": True
+    },
+    "Args": {
+        "description": "Function arguments",
+        "statement": False
+    },
+    "arguments": {
+        "description": "Function argument definitions",
+        "statement": False
+    },
+
     # Variables and Assignments
-    "Assign": "Assignment operation [S]",
-    "AnnAssign": "Annotated assignment [S]",
-    "AugAssign": "Augmented assignment (+=, -=, etc) [S]",
-    "Name": "Variable or function name [E]",
-    "Attribute": "Attribute access (obj.attr) [E]",
-    
+    "Assign": {
+        "description": "Assignment operation",
+        "statement": True
+    },
+    "AnnAssign": {
+        "description": "Annotated assignment",
+        "statement": True
+    },
+    "AugAssign": {
+        "description": "Augmented assignment (+=, -=, etc)",
+        "statement": True
+    },
+    "Name": {
+        "description": "Variable or function name",
+        "statement": False
+    },
+    "Attribute": {
+        "description": "Attribute access (obj.attr)",
+        "statement": False
+    },
+
     # Control Flow
-    "If": "If conditional statement [S]",
-    "For": "For loop [S]",
-    "AsyncFor": "Async for loop [S]",
-    "While": "While loop [S]",
-    "Break": "Break statement [S]",
-    "Continue": "Continue statement [S]",
-    "Try": "Try block [S]",
-    "TryStar": "Try block with star [S]",
-    "ExceptHandler": "Except clause [S]",
-    "With": "With statement [S]",
-    "AsyncWith": "Async with statement [S]",
-    "Match": "Pattern matching (Python 3.10+) [S]",
-    
+    "If": {
+        "description": "If conditional statement",
+        "statement": True
+    },
+    "For": {
+        "description": "For loop",
+        "statement": True
+    },
+    "AsyncFor": {
+        "description": "Async for loop",
+        "statement": True
+    },
+    "While": {
+        "description": "While loop",
+        "statement": True
+    },
+    "Break": {
+        "description": "Break statement",
+        "statement": True
+    },
+    "Continue": {
+        "description": "Continue statement",
+        "statement": True
+    },
+    "Try": {
+        "description": "Try block",
+        "statement": True
+    },
+    "TryStar": {
+        "description": "Try block with star",
+        "statement": True
+    },
+    "ExceptHandler": {
+        "description": "Except clause",
+        "statement": True
+    },
+    "With": {
+        "description": "With statement",
+        "statement": True
+    },
+    "AsyncWith": {
+        "description": "Async with statement",
+        "statement": True
+    },
+    "Match": {
+        "description": "Pattern matching (Python 3.10+)",
+        "statement": True
+    },
+
     # Expressions
-    "Expr": "Expression statement [S]",
-    "Call": "Function call [E]",
-    "Constant": "Literal constant [E]",
-    "List": "List literal [E]",
-    "Tuple": "Tuple literal [E]",
-    "Dict": "Dictionary literal [E]",
-    "Set": "Set literal [E]",
-    "ListComp": "List comprehension [E]",
-    "SetComp": "Set comprehension [E]",
-    "DictComp": "Dictionary comprehension [E]",
-    "GeneratorExp": "Generator expression [E]",
-    "Lambda": "Lambda expression [E]",
-    
+    "Expr": {
+        "description": "Expression statement",
+        "statement": True
+    },
+    "Call": {
+        "description": "Function call",
+        "statement": False
+    },
+    "Constant": {
+        "description": "Literal constant",
+        "statement": False
+    },
+    "List": {
+        "description": "List literal",
+        "statement": False
+    },
+    "Tuple": {
+        "description": "Tuple literal",
+        "statement": False
+    },
+    "Dict": {
+        "description": "Dictionary literal",
+        "statement": False
+    },
+    "Set": {
+        "description": "Set literal",
+        "statement": False
+    },
+    "ListComp": {
+        "description": "List comprehension",
+        "statement": False
+    },
+    "SetComp": {
+        "description": "Set comprehension",
+        "statement": False
+    },
+    "DictComp": {
+        "description": "Dictionary comprehension",
+        "statement": False
+    },
+    "GeneratorExp": {
+        "description": "Generator expression",
+        "statement": False
+    },
+    "Lambda": {
+        "description": "Lambda expression",
+        "statement": False
+    },
+
     # Operators
-    "BoolOp": "Boolean operation (and, or) [E]",
-    "BinOp": "Binary operation (+, -, *, /) [E]",
-    "UnaryOp": "Unary operation (not, ~, +, -) [E]",
-    "Compare": "Comparison operation [E]",
-    
+    "BoolOp": {
+        "description": "Boolean operation (and, or)",
+        "statement": False
+    },
+    "BinOp": {
+        "description": "Binary operation (+, -, *, /)",
+        "statement": False
+    },
+    "UnaryOp": {
+        "description": "Unary operation (not, ~, +, -)",
+        "statement": False
+    },
+    "Compare": {
+        "description": "Comparison operation",
+        "statement": False
+    },
+
     # Special
-    "Delete": "Delete statement [S]",
-    "Assert": "Assert statement [S]",
-    "Raise": "Raise exception [S]",
-    "Pass": "Pass statement [S]",
-    "Yield": "Yield expression [E]",
-    "YieldFrom": "Yield From expression [E]",
-    "Await": "Await expression [E]",
-    "Global": "Global declaration [S]",
-    "Nonlocal": "Nonlocal declaration [S]",
-    
+    "Delete": {
+        "description": "Delete statement",
+        "statement": True
+    },
+    "Assert": {
+        "description": "Assert statement",
+        "statement": True
+    },
+    "Raise": {
+        "description": "Raise exception",
+        "statement": True
+    },
+    "Pass": {
+        "description": "Pass statement",
+        "statement": True
+    },
+    "Yield": {
+        "description": "Yield expression",
+        "statement": False
+    },
+    "YieldFrom": {
+        "description": "Yield From expression",
+        "statement": False
+    },
+    "Await": {
+        "description": "Await expression",
+        "statement": False
+    },
+    "Global": {
+        "description": "Global declaration",
+        "statement": True
+    },
+    "Nonlocal": {
+        "description": "Nonlocal declaration",
+        "statement": True
+    },
+
     # Subscripting
-    "Subscript": "Subscript operation [E]",
-    "Slice": "Slice operation [E]",
-    "Starred": "Starred expression (*args) [E]",
-    
+    "Subscript": {
+        "description": "Subscript operation",
+        "statement": False
+    },
+    "Slice": {
+        "description": "Slice operation",
+        "statement": False
+    },
+    "Starred": {
+        "description": "Starred expression (*args)",
+        "statement": False
+    },
+
     # Comprehension parts
-    "comprehension": "Comprehension clauses [E]",
-    "alias": "Import alias [E]"
+    "comprehension": {
+        "description": "Comprehension clauses",
+        "statement": False
+    },
+    "alias": {
+        "description": "Import alias",
+        "statement": False
+    }
 }
