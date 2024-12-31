390722e20ceae4a8e379cb1d62402d9dd43e89ed
João Lopes <299513-kairos@users.noreply.replit.com>
2024-12-31 23:34:54 +0000

    1.0.18

--- scripts/update_workflows.py
+++ scripts/update_workflows.py
@@ -1,4 +1,3 @@
-
 """Update workflows configuration."""
 
 import subprocess
@@ -11,28 +10,41 @@ except Exception:
 
 workflows = [
     {
-        "name": "[Package] pypi upload",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Package] pypi upload",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
-            "task": "shell.exec",
-            "args": "python scripts/pypi_upload.py | tee logs/pypi_upload.log 2>&1"
+            "task":
+            "shell.exec",
+            "args":
+            "python scripts/pypi_upload.py | tee logs/pypi_upload.log 2>&1"
         }],
     },
     {
-        "name": "[Package] git commit",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Package] git commit",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
-            "task": "shell.exec",
-            "args": "python scripts/git_commit.py | tee logs/git_commit.log 2>&1"
+            "task":
+            "shell.exec",
+            "args":
+            "python scripts/git_commit.py | tee logs/git_commit.log 2>&1"
         }],
     },
     {
         "name": "————————————————",
         "mode": "sequential",
         "author": author_id,
-        "tasks": [{"task": "shell.exec", "args": ""}],
+        "tasks": [{
+            "task": "shell.exec",
+            "args": ""
+        }],
     },
     {
         "name": "[Util] create zip",
@@ -44,84 +56,139 @@ workflows = [
         }],
     },
     {
-        "name": "[Util] build",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Util] build",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
-            "task": "shell.exec",
-            "args": "rm -rf dist build *.egg-info && python setup.py sdist bdist_wheel"
+            "task":
+            "shell.exec",
+            "args":
+            "rm -rf dist build *.egg-info && python setup.py sdist bdist_wheel"
         }],
     },
     {
-        "name": "[Util] tests",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Util] tests",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
             "task": "shell.exec",
             "args": "pytest ./tests | tee logs/tests.log 2>&1"
         }],
     },
     {
-        "name": "[Util] demo",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Util] demo",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
             "task": "shell.exec",
             "args": "python -m examples | tee logs/demo.log 2>&1"
         }],
     },
     {
-        "name": "[Util] update workflows",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Util] update workflows",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
-            "task": "shell.exec",
-            "args": "python scripts/update_workflows.py | tee logs/update_worflows.log 2>&1"
+            "task":
+            "shell.exec",
+            "args": ("python scripts/update_workflows.py | tee " +
+                     "logs/update_worflows.log 2>&1")
         }],
     },
     {
         "name": "————————————————",
         "mode": "sequential",
         "author": author_id,
-        "tasks": [{"task": "shell.exec", "args": ""}],
+        "tasks": [{
+            "task": "shell.exec",
+            "args": ""
+        }],
     },
     {
         "name": "[Format] ruff",
         "mode": "sequential",
         "author": author_id,
-        "tasks": [{"task": "shell.exec", "args": "ruff format"}],
+        "tasks": [{
+            "task": "shell.exec",
+            "args": "ruff format"
+        }],
     },
     {
         "name": "————————————————",
         "mode": "sequential",
         "author": author_id,
-        "tasks": [{"task": "shell.exec", "args": ""}],
+        "tasks": [{
+            "task": "shell.exec",
+            "args": ""
+        }],
     },
     {
-        "name": "[Report] pyright",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Report] pyright",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
             "task": "shell.exec",
             "args": "pyright --warnings | tee logs/pyright.log 2>&1"
         }],
     },
     {
-        "name": "[Report] flake8",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Report] flake8",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
-            "task": "shell.exec",
-            "args": "flake8 --exclude */. --exclude ./build | tee logs/flake8.log 2>&1"
+            "task":
+            "shell.exec",
+            "args":
+            "flake8 --exclude */. --exclude ./build | tee logs/flake8.log 2>&1"
         }],
     },
     {
-        "name": "[Report] ruff",
-        "mode": "sequential",
-        "author": author_id,
+        "name":
+        "[Report] ruff",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
         "tasks": [{
-            "task": "shell.exec",
-            "args": "ruff check ./dev ./src ./examples ./tests ./scripts | tee logs/ruff.log 2>&1"
+            "task":
+            "shell.exec",
+            "args": ("ruff check ./dev ./src ./examples ./tests ./scripts | " +
+                     "tee logs/ruff.log 2>&1")
+        }],
+    },
+    {
+        "name":
+        "[Report] All",
+        "mode":
+        "sequential",
+        "author":
+        author_id,
+        "tasks": [{
+            "task":
+            "shell.exec",
+            "args": ("pyright --warnings | tee logs/pyright.log 2>&1 && " +
+                     "flake8 --exclude */. --exclude ./build | " +
+                     "tee logs/flake8.log 2>&1 && " +
+                     "ruff check ./dev ./src ./examples ./tests ./scripts | " +
+                     "tee logs/ruff.log 2>&1")
         }],
     },
 ]
